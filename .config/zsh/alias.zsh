c593e54b6b4a5cfc58615ab7a0050c0d1825369d
Lazydev0 <sangamzaki@gmail.com>
2026-01-05 19:59:40 +0530

    Changes-9

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,4 +1,5 @@
 # Essentials
+alias sudo='doas'
 alias ip='ip -color'
 alias neofetch='catnap'
 alias ping='ping -c 5'
@@ -18,10 +19,11 @@ alias fetch='fm6000 -wally -c yellow -n -g 12 -l 16 --not-de'
 alias paclock='sudo rm /var/lib/pacman/db.lck'
 alias kill_orphans='sudo pacman -Rns $(pacman -Qdtq)'
 alias kill_journal='sudo journalctl --vacuum-size=100M'
-alias kill_pkgcache="sudo pacman -Scc"
+alias kill_pkgcache="sudo paccache -r"
 alias kill_usrcache="rm -rf ~/.cache/*"
 alias kill_wallcache='rm -rf ~/.cache/wall_cache'
 alias kill_tmp="sudo rm -rf /tmp/* /var/tmp/*"
+alias kill_parucache="sudo rm -rf $HOME/.cache/paru/clone/*"
 alias kill_logs="sudo find /var/log -type f -name '*.log' ! -name 'pacman.log' -delete"
 alias kill_screenshots="rm -rf $HOME/Pictures/Screenshots/*"
 alias gg='git-graph --model simple'
--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -53,11 +53,3 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
-
-
-
-
-
-
-
-
