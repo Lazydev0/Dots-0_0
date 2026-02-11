ae966f4a1fb881501bebf1f8cc0c11d1e0b8367f
Lazydev0 <sangamzaki@gmail.com>
2026-02-11 17:05:17 +0530

    Changes-25

---
+++ .config/scripts/user/powermenu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+lock=" "
+logout=" "
+shutdown=" "
+reboot=" "
+sleep=" "
+
+selected_option=$(printf "%s\n" \
+  "$lock" \
+  "$logout" \
+  "$sleep" \
+  "$reboot" \
+  "$shutdown" | rofi -dmenu -i -p "Power" \
+  -theme ~/.config/rofi/powermenu.rasi)
+
+case "$selected_option" in
+"$lock")
+  hyprlock
+  ;;
+"$logout")
+  hyprctl dispatch exit
+  ;;
+"$shutdown")
+  mpv --no-video --quiet ~/.config/sounds/shutdown.mp3
+  systemctl poweroff
+  ;;
+"$reboot")
+  systemctl reboot
+  ;;
+"$sleep")
+  systemctl suspend
+  ;;
+*)
+  exit 0
+  ;;
+esac
