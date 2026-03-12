a31e130e221ae16d21a10a39a302ec1f0041d6d7
Lazydev0 <sangamzaki@gmail.com>
2026-03-12 12:31:10 +0530

    Changes-31

--- .config/zsh/functions/enva.zsh
+++ .config/zsh/functions/enva.zsh
@@ -1,4 +1,4 @@
-# Create python virtual enva
+# Create python3.11 virtual enva
 enva() {
     local env_name="${1:-venv}"
 
@@ -6,7 +6,7 @@ enva() {
         echo "Virtual environment '$env_name' already exists. Activating it..."
     else
         echo "Creating virtual environment '$env_name'..."
-        python -m venv "$env_name"
+        python3.11 -m venv "$env_name"
     fi
 
     source "$env_name/bin/activate"
